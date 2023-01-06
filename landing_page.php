35748d9ee707d69cc15ec715942abdc9acddec00
Ahmad Haidar Abdullah <72864321+AhmadHdr@users.noreply.github.com>
2023-01-06 09:21:58 +0700

    Add files via upload

---
+++ landing_page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css">
+</head>
+
+<body class="bg-gray-100 h-screen flex flex-col justify-center items-center">
+    <h1 class="text-3xl font-bold text-gray-800 mb-6">Welcome to the Landing Page</h1>
+    <div class="w-full max-w-xs">
+        <div class="bg-white rounded shadow-md py-4 px-8">
+            <div class="mb-4">
+                <a href="/login" class="btn btn-blue w-full">Login</a>
+            </div>
+            <div>
+                <a href="/register" class="btn btn-green w-full">Create an Account</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
